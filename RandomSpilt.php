b7f10e72875123412da4b2b20c05fe89bfed2448
LXZ <Lampxiezi@163.com>
2018-01-08 18:01:33 +0800

    update

--- RandomSpilt.php
+++ RandomSpilt.php
@@ -2,12 +2,15 @@
 
 use Phpml\CrossValidation\RandomSplit;
 use Phpml\Dataset\ArrayDataset;
+use Phpml\Metric\Accuracy;
+use Phpml\Regression\SVR;
+use Phpml\SupportVectorMachine\Kernel;
 
 require './vendor/autoload.php';
 
 $dataset = new ArrayDataset(
-    $samples = [[1], [2], [3], [4], [5], [6], [7], [8],[9],[10]],
-    $targets = ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b','c','c']
+    $samples = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]],
+    $targets = ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'c', 'c']
 );
 //第二个参数 是按照百分比 去分割的
 //第三个参数 是播下一个更好的随机数发生器种子 是 mt_stand() 这个函数
@@ -22,3 +25,4 @@ print_r($dataset_demp->getTrainLabels()); //训练的标记
 echo '<pre>';
 print_r($dataset_demp->getTestSamples()); //测试的数据
 print_r($dataset_demp->getTestLabels()); //测试的标记
+
--- StratifiedRandomSplit.php
+++ StratifiedRandomSplit.php
@@ -1,25 +1,34 @@
 <?php
 
 
+use pf\arr\PFarr;
 use Phpml\CrossValidation\StratifiedRandomSplit;
 use Phpml\Dataset\ArrayDataset;
+use Phpml\Dataset\Demo\WineDataset;
+use Phpml\Metric\Accuracy;
+use Phpml\Regression\SVR;
+use Phpml\SupportVectorMachine\Kernel;
 
 require './vendor/autoload.php';
 
-$dataset = new ArrayDataset(
-    $samples = [[1], [2], [3], [4], [5], [6], [7], [8],[9],[10]],
-    $targets = ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b','c','c']
-);
-
+$dataset = new WineDataset();
 $split = new StratifiedRandomSplit($dataset, 0.3);
-
 
 //随机取得训练数据
 echo '<pre>';
-print_r($split->getTrainSamples()); //训练的数据
-print_r($split->getTrainLabels()); //训练的标记
+//print_r($split->getTrainSamples()); //训练的数据
+//print_r($split->getTrainLabels()); //训练的标记
 
 //随机取得测试数据
 echo '<pre>';
-print_r($split->getTestSamples()); //测试的数据
-print_r($split->getTestLabels()); //测试的标记
+//print_r($split->getTestSamples()); //测试的数据
+//print_r($split->getTestLabels()); //测试的标记
+
+$regression = new SVR(Kernel::RBF, 3, 0.1, 10);
+$regression->train($split->getTrainSamples(), $split->getTrainLabels());
+$predicted = $regression->predict($split->getTestSamples());
+
+foreach ($predicted as &$target) {
+    $target = round($target, 0);
+}
+echo 'Accuracy: ' . Accuracy::score($split->getTestLabels(), $predicted);
